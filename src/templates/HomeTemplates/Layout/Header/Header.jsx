1639c73d2973c381e038bf4a9b5cb876ec3bfe4e
SnailT Dang <xuanthanh.110699@gmail.com>
2024-06-18 01:52:44 +0700

    removed found page

--- src/templates/HomeTemplates/Layout/Header/Header.jsx
+++ src/templates/HomeTemplates/Layout/Header/Header.jsx
@@ -1,27 +1,26 @@
-import React, { useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
-import { USER_LOGIN } from '../../../../constants/baseSettings/settings'
-import StatusUser from './StatusUser/StatusUser'
-import 'tippy.js/dist/tippy.css';
-import { useSelector } from 'react-redux';
-import { checkLogin } from '../../../../App';
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { USER_LOGIN } from "../../../../constants/baseSettings/settings";
+import StatusUser from "./StatusUser/StatusUser";
+import "tippy.js/dist/tippy.css";
+import { useSelector } from "react-redux";
+import { checkLogin } from "../../../../App";
 
 const isActiveMenu = (isActive) => {
     if (!isActive) {
-        return "nav-link flex items-center p-5 text-white text-lg unselected font-semibold hover:text-orange-main"
+        return "nav-link flex items-center p-5 text-white text-lg unselected font-semibold hover:text-orange-main";
     } else {
-        return "nav-link flex items-center p-5 text-lg font-semibold text-orange-main border-orange-main hover:text-orange-main"
+        return "nav-link flex items-center p-5 text-lg font-semibold text-orange-main border-orange-main hover:text-orange-main";
     }
-}
-
-
+};
 
 export default function Header(props) {
-    const {userLogin} = useSelector(state => state.userReducer)
-    const isLogin = checkLogin()
-    let userHeader = isLogin? JSON.parse(localStorage.getItem(USER_LOGIN)) || userLogin : null
-    useEffect (()=> {
-    },[])
+    const { userLogin } = useSelector((state) => state.userReducer);
+    const isLogin = checkLogin();
+    let userHeader = isLogin
+        ? JSON.parse(localStorage.getItem(USER_LOGIN)) || userLogin
+        : null;
+    useEffect(() => {}, []);
     return (
         <>
             <header className="pb-5 bg-blue-main bg-opacity-500/75 text-coolGray-100 w-100 z-10000 text-white sticky top-0">
@@ -29,7 +28,8 @@ export default function Header(props) {
                     <div className="header-container container">
                         <div className="header-right">
                             <div className="header-phone text-right">
-                                Hotline: <a href="tel:02363630689">0236 3630 689</a>
+                                Hotline:{" "}
+                                <a href="tel:02363630689">0236 3630 689</a>
                             </div>
                             <div className="header-top-time text-right">
                                 <div className="time-header">
@@ -41,43 +41,80 @@ export default function Header(props) {
                 </div>
                 <div className="bg-strong-blue bg-opacity-500/75">
                     <div className="container flex justify-between mx-auto relative">
-                        <a href='/' className="block w-fit items-center absolute -top-14 left-4 -bottom-4 bg-strong-blue bg-opacity-500/75">
-                            <img src={'https://snailtdang.github.io/cyberphone/img/Brandlogo.png'} alt="" className='w-32' />
+                        <a
+                            href="/"
+                            className="block w-fit items-center absolute -top-14 left-4 -bottom-4 bg-strong-blue bg-opacity-500/75"
+                        >
+                            <img
+                                src={
+                                    "https://snailtdang.github.io/cyberphone/img/Brandlogo.png"
+                                }
+                                alt=""
+                                className="w-32"
+                            />
                         </a>
                         <ul className="items-stretch space-x-3 flex mb-0 ml-20 lg:ml-64">
                             <li className="flex">
-                                <NavLink to="/"
-                                    className={isActive => isActiveMenu(isActive.isActive)}
-                                >HOME</NavLink>
+                                <NavLink
+                                    to="/"
+                                    className={(isActive) =>
+                                        isActiveMenu(isActive.isActive)
+                                    }
+                                >
+                                    HOME
+                                </NavLink>
                             </li>
                             <li className="flex">
-                                <NavLink to="/showtimes" className={isActive => isActiveMenu(isActive.isActive)}>SHOWTIMES</NavLink>
+                                <NavLink
+                                    to="/showtimes"
+                                    className={(isActive) =>
+                                        isActiveMenu(isActive.isActive)
+                                    }
+                                >
+                                    SHOWTIMES
+                                </NavLink>
                             </li>
                             <li className="flex">
-                                <NavLink to="/news"
-                                    className={isActive => isActiveMenu(isActive.isActive)}
-                                >PROMOTIONS</NavLink>
+                                <NavLink
+                                    to="/news"
+                                    className={(isActive) =>
+                                        isActiveMenu(isActive.isActive)
+                                    }
+                                >
+                                    PROMOTIONS
+                                </NavLink>
                             </li>
                             <li className="flex">
-                                <NavLink to="/contact"
-                                    className={isActive => isActiveMenu(isActive.isActive)}
-                                >CONTACT</NavLink>
-                            </li>
-                            <li className="flex">
-                                <NavLink to="/login"
-                                    className={isActive => isActiveMenu(isActive.isActive)}
-                                >LOGIN</NavLink>
+                                <NavLink
+                                    to="/contact"
+                                    className={(isActive) =>
+                                        isActiveMenu(isActive.isActive)
+                                    }
+                                >
+                                    CONTACT
+                                </NavLink>
                             </li>
                         </ul>
                         <StatusUser user={userHeader} />
                         <button className="p-4 lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-coolGray-100">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                className="w-6 h-6 text-coolGray-100"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
                             </svg>
                         </button>
                     </div>
                 </div>
-            </header >
+            </header>
         </>
-    )
+    );
 }
